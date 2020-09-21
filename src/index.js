efbe6636aa5b1536cda8eabede1705d955851f85
alitu <alitu14@gmail.com>
2020-09-21 23:02:59 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import  './css/style.css';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
